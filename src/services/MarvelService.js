5158c452fefa88454d8d004b5f18d5914c224e09
Kerim <kkerimow123@gmail.com>
2024-01-17 22:53:07 +0300

    add search character form

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -25,6 +25,11 @@ const useMarvelService = () => {
         return res.data.results.map(_transformCharacter);
     }
 
+    const getCharacterByName = async (name) => {
+		const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+		return res.data.results.map(_transformCharacter);
+	};
+
     const getCharacter = async (id) => {
         const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
         return _transformCharacter(res.data.results[0]);
@@ -71,7 +76,7 @@ const useMarvelService = () => {
 		};
 	};
 
-    return {loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComics,};
+    return {loading, error, getAllCharacters, getCharacterByName, getCharacter, clearError, getAllComics, getComics,};
 }
 
 export default useMarvelService;
